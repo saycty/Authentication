d24fff9d2aa69be2fea67ba77eebcd4be7c5df0f
sayan chakrabortty <sayanchakrabortty3@gmail.com>
2023-11-19 18:26:33 +0530

    updade db

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
-//mongodb+srv://sayanchakrabortty3:1234@cluster0.kihl4gs.mongodb.net/?retryWrites=true&w=majority
 
 import mongoose from "mongoose";
- 
-const DB=async()=>{
-    try {
-        await mongoose.connect('mongodb+srv://sayanchakrabortty3:1234@cluster0.kihl4gs.mongodb.net/?retryWrites=true&w=majority');
+
+const DB = async (mongoURI) => {
+    if (!mongoURI) {
+        // throw new Error('MongoDB connection string is not defined.');
+        res.send('database not matched');
+    }
+    await mongoose.connect(mongoURI, {
+    });
+
     console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
+
 };
 
-export default DB;  
+export default DB;
